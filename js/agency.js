4cf542c8f1d76b9f4d81520027dedd6b74d8b28a
honghao bi <henrybidesign@gmail.com>
2017-02-26 23:46:24 -0800

    about page created

--- js/agency.js
+++ js/agency.js
@@ -32,4 +32,11 @@
         }
     });
 
+    $('.flip').click(function(){
+        $(this).find('.card').addClass('flipped').mouseleave(function(){
+            $(this).removeClass('flipped');
+        });
+        return false;
+    });
+
 })(jQuery); // End of use strict
